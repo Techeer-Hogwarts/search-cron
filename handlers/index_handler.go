e79a641bf4262029ead25f82a00fc97b626633b8
Ryan <leehahnwoong@gmail.com>
2025-09-01 15:08:45 -0700

    feat: add prometheus metrics to search cron

--- handlers/index_handler.go
+++ handlers/index_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Techeer-Hogwarts/search-cron/models"
 	"github.com/Techeer-Hogwarts/search-cron/services"
@@ -32,20 +33,25 @@ func NewIndexHandler(service services.IndexService) *IndexHandler {
 // @Router /index/user [post]
 func (h *IndexHandler) CreateUserIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
 	// Create index in meilisearch
+	startTime := time.Now()
 	var userIndex models.UserIndex
 
 	if err := c.ShouldBindJSON(&userIndex); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "user").Inc()
 		return
 	}
 
 	info, err := h.service.CreateUserIndex(&userIndex)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "user").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("User Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "user").Inc()
+	durationHistogram.WithLabelValues("success", "user").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -62,20 +68,25 @@ func (h *IndexHandler) CreateUserIndexHandler(c *gin.Context, counter *prometheu
 // @Router /index/project [post]
 func (h *IndexHandler) CreateProjectIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
 	// Create index in meilisearch
+	startTime := time.Now()
 	var projectIndex models.ProjectIndex
 
 	if err := c.ShouldBindJSON(&projectIndex); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "project").Inc()
 		return
 	}
 
 	info, err := h.service.CreateProjectIndex(&projectIndex)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "project").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Project Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "project").Inc()
+	durationHistogram.WithLabelValues("success", "project").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -92,20 +103,25 @@ func (h *IndexHandler) CreateProjectIndexHandler(c *gin.Context, counter *promet
 // @Router /index/study [post]
 func (h *IndexHandler) CreateStudyIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
 	// Create index in meilisearch
+	startTime := time.Now()
 	var studyIndex models.StudyIndex
 
 	if err := c.ShouldBindJSON(&studyIndex); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "study").Inc()
 		return
 	}
 
 	info, err := h.service.CreateStudyIndex(&studyIndex)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "study").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Study Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "study").Inc()
+	durationHistogram.WithLabelValues("success", "study").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -122,20 +138,25 @@ func (h *IndexHandler) CreateStudyIndexHandler(c *gin.Context, counter *promethe
 // @Router /index/blog [post]
 func (h *IndexHandler) CreateBlogIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
 	// Create index in meilisearch
+	startTime := time.Now()
 	var blogIndex models.BlogIndex
 
 	if err := c.ShouldBindJSON(&blogIndex); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "blog").Inc()
 		return
 	}
 
 	info, err := h.service.CreateBlogIndex(&blogIndex)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "blog").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Blog Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "blog").Inc()
+	durationHistogram.WithLabelValues("success", "blog").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -152,20 +173,25 @@ func (h *IndexHandler) CreateBlogIndexHandler(c *gin.Context, counter *prometheu
 // @Router /index/resume [post]
 func (h *IndexHandler) CreateResumeIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
 	// Create index in meilisearch
+	startTime := time.Now()
 	var resumeIndex models.ResumeIndex
 
 	if err := c.ShouldBindJSON(&resumeIndex); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "resume").Inc()
 		return
 	}
 
 	info, err := h.service.CreateResumeIndex(&resumeIndex)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "resume").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Resume Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "resume").Inc()
+	durationHistogram.WithLabelValues("success", "resume").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -182,20 +208,25 @@ func (h *IndexHandler) CreateResumeIndexHandler(c *gin.Context, counter *prometh
 // @Router /index/session [post]
 func (h *IndexHandler) CreateSessionIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
 	// Create index in meilisearch
+	startTime := time.Now()
 	var sessionIndex models.SessionIndex
 
 	if err := c.ShouldBindJSON(&sessionIndex); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "session").Inc()
 		return
 	}
 
 	info, err := h.service.CreateSessionIndex(&sessionIndex)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "session").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Session Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "session").Inc()
+	durationHistogram.WithLabelValues("success", "session").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -212,20 +243,25 @@ func (h *IndexHandler) CreateSessionIndexHandler(c *gin.Context, counter *promet
 // @Router /index/event [post]
 func (h *IndexHandler) CreateEventIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
 	// Create index in meilisearch
+	startTime := time.Now()
 	var eventIndex models.EventIndex
 
 	if err := c.ShouldBindJSON(&eventIndex); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "event").Inc()
 		return
 	}
 
 	info, err := h.service.CreateEventIndex(&eventIndex)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "event").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Event Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "event").Inc()
+	durationHistogram.WithLabelValues("success", "event").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -240,13 +276,17 @@ func (h *IndexHandler) CreateEventIndexHandler(c *gin.Context, counter *promethe
 // @Failure 500 {object} map[string]interface{}
 // @Router /index/stack [post]
 func (h *IndexHandler) CreateStackIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
+	startTime := time.Now()
 	info, err := h.service.CreateStackIndex()
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "stack").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Stack Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "stack").Inc()
+	durationHistogram.WithLabelValues("success", "stack").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -366,13 +406,17 @@ func (h *IndexHandler) DeleteAllDocumentHandler(c *gin.Context) {
 // @Failure 500 {object} map[string]interface{}
 // @Router /sync/all [post]
 func (h *IndexHandler) SyncAllIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
+	startTime := time.Now()
 	info, err := h.service.CreateAllIndex("all")
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "syncall").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "syncall").Inc()
+	durationHistogram.WithLabelValues("success", "syncall").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
 
@@ -387,12 +431,16 @@ func (h *IndexHandler) SyncAllIndexHandler(c *gin.Context, counter *prometheus.C
 // @Failure 500 {object} map[string]interface{}
 // @Router /sync/new [post]
 func (h *IndexHandler) SyncNewIndexHandler(c *gin.Context, counter *prometheus.CounterVec, durationHistogram *prometheus.HistogramVec) {
+	startTime := time.Now()
 	info, err := h.service.CreateAllIndex("new")
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
+		counter.WithLabelValues("failure", "syncnew").Inc()
 		return
 	}
 	jsonData, _ := json.MarshalIndent(info, "", "  ")
 	log.Printf("Index created successfully: %v", string(jsonData))
+	counter.WithLabelValues("success", "syncnew").Inc()
+	durationHistogram.WithLabelValues("success", "syncnew").Observe(time.Since(startTime).Seconds())
 	c.JSON(200, gin.H{"message": "Index created successfully", "info": info})
 }
